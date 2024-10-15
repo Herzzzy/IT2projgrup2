d45993d4bec7201f2849d0685215a2bed42a9f4d
Herzzzy <95689436+Herzzzy@users.noreply.github.com>
2024-10-15 13:33:10 +0200

    Add files via upload

---
+++ IT2proj.py
@@ -0,0 +1,16 @@
+import matplotlib.pylab as plt
+import numpy as np
+
+xverdier = np.linspace(1,100,100)
+
+yverdier = 0.5*xverdier**2
+plt.subplot(2, 1, 1)
+plt.plot(xverdier, yverdier)
+plt.grid()
+
+yverdier = -0.3*xverdier**3
+plt.subplot(2,1,2)
+plt.plot(xverdier, yverdier)
+plt.grid()
+
+plt.show()
