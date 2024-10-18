812d52a4466ea96613c73893b0cd884285894e14
PrebenKibs <preben.kibs@gmail.com>
2024-10-18 14:03:27 +0200

    Update IT2proj.py

--- IT2proj.py
+++ IT2proj.py
@@ -25,3 +25,9 @@ with open(filnavn, encoding="utf-8-sig") as fil:
     print("Har denne sangen flere eller færre avspillninger enn?", bestemt_rad2[artist_index])
     
     bruker_gjett = int(input("Trykk h for Høyere, og l for Lavere")
+
+    if (bruker_gjett == 'h' and int(bestemt_rad2[total_streams_index]) > int(bestemt_rad[total_streams_index])) or \
+       (bruker_gjett == 'l' and int(bestemt_rad2[total_streams_index]) < int(bestemt_rad[total_streams_index])):
+        print("Riktig! {} har {} avspillninger.".format(bestemt_rad2[artist_index], bestemt_rad2[total_streams_index]))
+    else:
+        print("Feil! {} har {} avspillninger.".format(bestemt_rad2[artist_index], bestemt_rad2[total_streams_index]))
