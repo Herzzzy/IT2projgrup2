5482d29b2790783ab1111919af7b54d0226a1b67
Herzzzy <95689436+Herzzzy@users.noreply.github.com>
2024-10-18 13:31:05 +0200

    Update IT2proj.py

--- IT2proj.py
+++ IT2proj.py
@@ -1,1 +1,23 @@
+import csv
+import random
 
+filnavn = "spotify_data.csv"
+
+with open(filnavn, encoding="utf-8-sig") as fil:
+    filinnhold = csv.reader(fil, delimiter=";")
+
+    overskrifter = next(filinnhold)
+    print("Headers:", overskrifter)
+
+    total_streams_index = 2 
+    artist_index = 0  
+
+    rad = list(filinnhold)
+
+    if len(rad) > 0:
+        rad_index = random.randint(0, len(rad) - 1)
+        bestemt_rad = rad[rad_index]
+
+        print("Artist:", bestemt_rad[artist_index], "Total Streams:", bestemt_rad[total_streams_index])
+    else:
+        print("No data rows found in the CSV.")
