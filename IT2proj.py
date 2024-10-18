08a2441128f9bbe03285d125029213d8824e6f90
Martin-h-n <martinneubeck@gmail.com>
2024-10-18 12:50:43 +0200

    Update IT2proj.py

--- IT2proj.py
+++ IT2proj.py
@@ -1,18 +1,1 @@
-import matplotlib.pylab as plt
-import numpy as np
-#testing...
-xverdier = np.linspace(1,100,100)
 
-#hello world!
-
-yverdier = 0.5*xverdier**2
-plt.subplot(2, 1, 1)
-plt.plot(xverdier, yverdier)
-plt.grid()
-
-yverdier = -0.3*xverdier**3
-plt.subplot(2,1,2)
-plt.plot(xverdier, yverdier)
-plt.grid()
-
-plt.show()
